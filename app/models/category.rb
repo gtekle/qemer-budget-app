74ea330ff0c15ce8606290901036b1fc40de07de
tYohannes <teklegy@gmail.com>
2022-05-23 16:10:14 +0300

    Add ExpenseCategory model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :expense_categories, dependent: :delete_all
 
   validates :name, presence: true
   validates :icon, presence: true
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,5 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  has_many :expense_categories, dependent: :delete_all
 
   validates :name, presence: true
   validates :amount, comparison: { greater_than: 0.0 }, numericality: true
