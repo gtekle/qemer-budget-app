4858e0e280eec90f65e075833353b4ca66d98504
tYohannes <teklegy@gmail.com>
2022-05-23 17:43:32 +0300

    Setup Devise authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :categories, foreign_key: 'author_id'
   has_many :expenses, foreign_key: 'author_id'
 
