77a77945878737bd3ac76dee36468e46ee1e7bc3
tYohannes <teklegy@gmail.com>
2022-05-23 22:45:50 +0300

    Add Categories controller with resource routes

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :categories, only: %i[index new create destroy]
 
   # Defines the root path route ("/")
   root "splash#index"
