c315ff99c473ce56f66dbe71846dd677996e42e6
tYohannes <teklegy@gmail.com>
2022-05-26 18:25:50 +0300

    Add devise mailer config for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "qemer_budget_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://qemer-budget-app.herokuapp.com/' }
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :user_name => "tekle.gebreyohannesk@gmail.com",
+    :password => "GENERATEDPASSWORD",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
